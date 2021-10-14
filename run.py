6ef3120d130e95847fc16b0f8d99ab0824efa494
taeukkkim <ktu9682@gmail.com>
2021-10-14 18:46:20 +0000

    [#4] make run.py

---
+++ run.py
@@ -0,0 +1,308 @@
+import logging
+import os
+import sys
+from functools import partial
+from typing import List, Callable, NoReturn, NewType, Any
+import dataclasses
+from datasets import load_metric, load_from_disk, Sequence, Value, Features, Dataset, DatasetDict
+
+from transformers import AutoConfig, AutoModelForQuestionAnswering, AutoTokenizer
+
+from transformers import (
+    DataCollatorWithPadding,
+    EvalPrediction,
+    TrainingArguments,
+    set_seed,
+)
+
+from tokenizers import Tokenizer
+from tokenizers.models import WordPiece
+
+from trainer_qa import QuestionAnsweringTrainer
+from retrieval import SparseRetrieval
+
+from solution.args import (
+    HfArgumentParser,
+    get_args_parser,
+    DataArguments,
+    ModelingArguments,
+)
+from solution.reader import (
+    prepare_train_features,
+    prepare_validation_features,
+    post_processing_function,
+)
+from solution.utils import (
+    compute_metrics,
+    check_no_error,
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    # 가능한 arguments 들은 ./arguments.py 나 transformer package 안의 src/transformers/training_args.py 에서 확인 가능합니다.
+    # --help flag 를 실행시켜서 확인할 수 도 있습니다.
+    command_args = get_args_parser()
+    parser = HfArgumentParser(
+        (ModelingArguments, DataArguments, TrainingArguments)
+    )
+    model_args, data_args, training_args = parser.parse_yaml_file(yaml_file=os.path.abspath(command_args.config))
+    print(model_args.model_name_or_path)
+
+    # [참고] argument를 manual하게 수정하고 싶은 경우에 아래와 같은 방식을 사용할 수 있습니다
+    # training_args.per_device_train_batch_size = 4
+    # print(training_args.per_device_train_batch_size)
+
+    print(f"model is from {model_args.model_name_or_path}")
+    print(f"data is from {data_args.dataset_name}")
+
+    # logging 설정
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -    %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    # verbosity 설정 : Transformers logger의 정보로 사용합니다 (on main process only)
+    logger.info("Training/evaluation parameters %s", training_args)
+
+    # 모델을 초기화하기 전에 난수를 고정합니다.
+    set_seed(training_args.seed)
+
+    datasets = load_from_disk(data_args.dataset_name)
+    print(datasets)
+
+    # AutoConfig를 이용하여 pretrained model 과 tokenizer를 불러옵니다.
+    # argument로 원하는 모델 이름을 설정하면 옵션을 바꿀 수 있습니다.
+    config = AutoConfig.from_pretrained(
+        model_args.config_name
+        if model_args.config_name
+        else model_args.model_name_or_path,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.tokenizer_name
+        if model_args.tokenizer_name
+        else model_args.model_name_or_path,
+        # 'use_fast' argument를 True로 설정할 경우 rust로 구현된 tokenizer를 사용할 수 있습니다.
+        # False로 설정할 경우 python으로 구현된 tokenizer를 사용할 수 있으며,
+        # rust version이 비교적 속도가 빠릅니다.
+        use_fast=True,
+    )
+    model = AutoModelForQuestionAnswering.from_pretrained(
+        model_args.model_name_or_path,
+        from_tf=bool(".ckpt" in model_args.model_name_or_path),
+        config=config,
+    )
+
+    print(
+        type(training_args),
+        type(model_args),
+        type(datasets),
+        type(tokenizer),
+        type(model),
+    )
+
+    # ==========================================
+    # True일 경우 : run passage retrieval
+    # if data_args.eval_retrieval:
+    #     datasets = run_sparse_retrieval(
+    #         tokenizer.tokenize,
+    #         datasets,
+    #         training_args,
+    #         data_args,
+    #     )
+    # ==========================================
+
+    # do_train mrc model 혹은 do_eval mrc model
+    if training_args.do_train or training_args.do_eval or training_args.do_predict:
+        run_mrc(data_args, training_args, model_args, datasets, tokenizer, model)
+
+# ====================================================
+# TODO 바꿔줄 함수
+def run_sparse_retrieval(
+    tokenize_fn: Callable[[str], List[str]],
+    datasets: DatasetDict,
+    training_args: TrainingArguments,
+    data_args: DataArguments,
+    data_path: str = "../data",
+    context_path: str = "wikipedia_documents.json",
+) -> DatasetDict:
+
+    # Query에 맞는 Passage들을 Retrieval 합니다.
+    retriever = SparseRetrieval(
+        tokenize_fn=tokenize_fn, data_path=data_path, context_path=context_path
+    )
+    retriever.get_sparse_embedding()
+
+    if data_args.use_faiss:
+        retriever.build_faiss(num_clusters=data_args.num_clusters)
+        df = retriever.retrieve_faiss(
+            datasets["validation"], topk=data_args.top_k_retrieval
+        )
+    else:
+        df = retriever.retrieve(datasets["validation"], topk=data_args.top_k_retrieval)
+
+    # test data 에 대해선 정답이 없으므로 id question context 로만 데이터셋이 구성됩니다.
+    if training_args.do_predict:
+        f = Features(
+            {
+                "context": Value(dtype="string", id=None),
+                "id": Value(dtype="string", id=None),
+                "question": Value(dtype="string", id=None),
+            }
+        )
+
+    # train data 에 대해선 정답이 존재하므로 id question context answer 로 데이터셋이 구성됩니다.
+    elif training_args.do_eval:
+        f = Features(
+            {
+                "answers": Sequence(
+                    feature={
+                        "text": Value(dtype="string", id=None),
+                        "answer_start": Value(dtype="int32", id=None),
+                    },
+                    length=-1,
+                    id=None,
+                ),
+                "context": Value(dtype="string", id=None),
+                "id": Value(dtype="string", id=None),
+                "question": Value(dtype="string", id=None),
+            }
+        )
+    datasets = DatasetDict({"validation": Dataset.from_pandas(df, features=f)})
+    return datasets
+# ====================================================
+
+def run_mrc(
+    data_args: DataArguments,
+    training_args: TrainingArguments,
+    model_args: ModelingArguments,
+    datasets: DatasetDict,
+    tokenizer,
+    model,
+) -> NoReturn:
+
+    # dataset을 전처리합니다.
+    # training과 evaluation에서 사용되는 전처리는 아주 조금 다른 형태를 가집니다.
+    if training_args.do_train:
+        column_names = datasets["train"].column_names
+    else:
+        column_names = datasets["validation"].column_names
+
+    # 오류가 있는지 확인합니다.
+    last_checkpoint, max_seq_length = check_no_error(
+        data_args, training_args, datasets, tokenizer
+    )
+
+    if training_args.do_train:
+        if "train" not in datasets:
+            raise ValueError("--do_train requires a train dataset")
+        train_dataset = datasets["train"]
+
+        # dataset에서 train feature를 생성합니다.
+        _prepare_train_features = partial(
+            prepare_train_features,
+            tokenizer=tokenizer,
+        )
+        train_dataset = train_dataset.map(
+            _prepare_train_features,
+            batched=True,
+            num_proc=data_args.preprocessing_num_workers,
+            remove_columns=column_names,
+            load_from_cache_file=not data_args.overwrite_cache,
+        )
+
+    if training_args.do_eval or training_args.do_predict:
+        eval_dataset = datasets["validation"]
+
+        # Validation Feature 생성
+        _prepare_validation_features = partial(
+            prepare_validation_features,
+            tokenizer=tokenizer,
+        )
+        eval_dataset = eval_dataset.map(
+            _prepare_validation_features,
+            batched=True,
+            num_proc=data_args.preprocessing_num_workers,
+            remove_columns=column_names,
+            load_from_cache_file=not data_args.overwrite_cache,
+        )
+
+    # Data collator
+    # flag가 True이면 이미 max length로 padding된 상태입니다.
+    # 그렇지 않다면 data collator에서 padding을 진행해야합니다.
+    data_collator = DataCollatorWithPadding(
+        tokenizer, pad_to_multiple_of=8 if training_args.fp16 else None
+    )
+
+    # Trainer 초기화
+    trainer = QuestionAnsweringTrainer( 
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset if training_args.do_train else None,
+        eval_dataset=eval_dataset if training_args.do_eval or training_args.do_predict else None,
+        eval_examples=datasets["validation"] if training_args.do_eval or training_args.do_predict else None,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        post_process_function=post_processing_function,
+        compute_metrics=compute_metrics,
+    )
+
+    # Training
+    if training_args.do_train:
+        if last_checkpoint is not None:
+            checkpoint = last_checkpoint
+        elif os.path.isdir(model_args.model_name_or_path):
+            checkpoint = model_args.model_name_or_path
+        else:
+            checkpoint = None
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        trainer.save_model()  # Saves the tokenizer too for easy upload
+
+        metrics = train_result.metrics
+        metrics["train_samples"] = len(train_dataset)
+
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+        output_train_file = os.path.join(training_args.output_dir, "train_results.txt")
+
+        with open(output_train_file, "w") as writer:
+            logger.info("***** Train results *****")
+            for key, value in sorted(train_result.metrics.items()):
+                logger.info(f"  {key} = {value}")
+                writer.write(f"{key} = {value}\n")
+
+        # State 저장
+        trainer.state.save_to_json(
+            os.path.join(training_args.output_dir, "trainer_state.json")
+        )
+
+    # Evaluation
+    logger.info("*** Evaluate ***")
+    
+    #### eval dataset & eval example - predictions.json 생성됨
+    if training_args.do_predict:
+        predictions = trainer.predict(
+            test_dataset=eval_dataset, test_examples=datasets["validation"]
+        )
+
+        # predictions.json 은 postprocess_qa_predictions() 호출시 이미 저장됩니다.
+        print(
+            "No metric can be presented because there is no correct answer given. Job done!"
+        )
+
+    if training_args.do_eval:
+        metrics = trainer.evaluate()
+
+        metrics["eval_samples"] = len(eval_dataset)
+
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
+
+if __name__ == "__main__":
+    main()
