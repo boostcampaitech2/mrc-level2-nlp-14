7a0732cf87dfc6a06ed1ebda0221e8a22f30b73e
MyungHoonJin <jinmang2@gmail.com>
2021-10-26 12:59:12 +0000

    [#9] fix model init function

--- solution/reader/architectures/__init__.py
+++ solution/reader/architectures/__init__.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from transformers import AutoConfig, PreTrainedModel
 from transformers import PreTrainedTokenizer
 
@@ -9,12 +11,20 @@ import sys
 mod = sys.modules[__name__]
 
 
+def _set_attr(main: Any, attrname: str, getfrom: Any):
+    args_value = getattr(getfrom, attrname, None)
+    args_value = getattr(main, attrname, args_value)
+    setattr(main, attrname, args_value)
+
+
 def _get_config(model_args: ModelArguments):
     config = AutoConfig.from_pretrained(
         model_args.config_name
         if model_args.config_name
         else model_args.model_name_or_path,
+        use_auth_token=model_args.use_auth_token,
     )
+    _set_attr(config, "reader_type", model_args)
     return config
 
 
@@ -28,8 +38,9 @@ def _get_model(
     model = model_cls.from_pretrained(
         model_args.model_name_or_path,
         config=config,
+        use_auth_token=model_args.use_auth_token,
     )
-    return model
+    return model    
 
 
 def basic(
@@ -47,14 +58,14 @@ def add_qaconv_head(
     default_model: PreTrainedModel,
     tokenizer: PreTrainedTokenizer,
 ):
-    assert model_args.reader_type != "extractive"
+    assert model_args.reader_type == "extractive"
     config = _get_config(model_args)
-    config.model_head = model_args.model_head
-    if "conv" in config.model_head == :
-        config.qa_conv_out_channel = model_args.qa_conv_out_channel
-        config.qa_conv_input_size = model_args.qa_conv_input_size
-        config.qa_conv_n_layers = model_args.qa_conv_n_layers
-    config.sep_token_id = tokenizer.sep_token_id
+    if "sds_conv" in model_args.model_head:
+        _set_attr(config, "qa_conv_input_size", model_args)
+        _set_attr(config, "qa_conv_n_layers", model_args)
+    elif "conv" in model_args.model_head:
+        _set_attr(config, "qa_conv_out_channel", model_args)
+        _set_attr(config, "sep_token_id", tokenizer)
     model = _get_model(model_args, default_model, config)
     return model
 
