a7d890edac3846456f646d226f680a56eb3a62ff
Haram Lee <twinha0204@gmail.com>
2021-10-29 11:27:25 +0000

    [#20] add ensemble trainer

--- solution/reader/trainers/__init__.py
+++ solution/reader/trainers/__init__.py
@@ -1,3 +1,4 @@
 from .base import BaseTrainer, Seq2SeqBaseTrainer
 from .qa import QuestionAnsweringTrainer
 from .seq2seq_qa import QuestionAnsweringSeq2SeqTrainer
+from .ensemble_qa import QuestionAnsweringEnsembleTrainer
