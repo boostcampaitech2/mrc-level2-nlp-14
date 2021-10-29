cd780a6de4000ede1cff0215f3f1eceb09691abd
Haram Lee <twinha0204@gmail.com>
2021-10-29 11:24:07 +0000

    [#20] add post processing function map

--- solution/data/processors/__init__.py
+++ solution/data/processors/__init__.py
@@ -1,2 +1,10 @@
 from .odqa import OdqaProcessor, convert_examples_to_features
-from .post import post_processing_function
+from .post import post_processing_function, gen_postprocessing_function
+
+
+POST_PROCESSING_FUNCTION = {
+    "extractive": post_processing_function,
+    "generative": gen_postprocessing_function,
+    "ensemble": gen_postprocessing_function,
+    "retro": post_processing_function,
+}
