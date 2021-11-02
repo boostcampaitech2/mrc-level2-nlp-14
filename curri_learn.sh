320902bb81ff1af75095e4c8738d4e0af21a704e
taeukkkim <ktu9682@gmail.com>
2021-11-02 12:44:16 +0000

    Fix for revision

--- curri_learn.sh
+++ curri_learn.sh
@@ -1,11 +1,11 @@
 # !bin/bash
 
-python3 new_run_curri.py configs/curri01/easy.yaml
+python3 new_run.py configs/curri01/easy.yaml
 
-rm -r outputs/tu_curri_test_e_noaug/trainer_state.json
+rm -r outputs/tu_curri_test_e_noaug_pororo_ver2/trainer_state.json
 
-python3 new_run_curri.py configs/curri01/normal.yaml
+python3 new_run.py configs/curri01/normal.yaml
 
-rm -r outputs/tu_curri_test_n_noaug/trainer_state.json
+rm -r outputs/tu_curri_test_n_noaug_pororo_ver2/trainer_state.json
 
-python3 new_run_curri.py configs/curri01/hard.yaml
+python3 new_run.py configs/curri01/hard.yaml
