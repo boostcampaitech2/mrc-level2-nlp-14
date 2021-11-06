ef6d45c1337c7d65e5d30747488e2d4efc4784f3
MyungHoonJin <jinmang2@gmail.com>
2021-11-06 23:45:16 +0900

    add help text on model_args

--- solution/reader/__init__.py
+++ solution/reader/__init__.py
@@ -2,12 +2,10 @@ from .readers import (
     ExtractiveReader,
     GenerativeReader,
     EnsembleReader,
-    RetroReader,
 )
 
 READER_HOST = {
     "extractive": ExtractiveReader,
     "generative": GenerativeReader,
     "ensemble": EnsembleReader,
-    "retro": RetroReader,
 }
